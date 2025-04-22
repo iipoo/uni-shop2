514bb8446abb20691022daa64aed17c72d29dcd4
Mr.Li <13381028836@189.cn>
2025-04-22 20:31:39 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,59 @@
+<template>
+	<view>
+		<view class="goods-item">
+			<view class="goods-left">
+				<image :src="goods.goods_small_logo || defaultPic"></image>
+			</view>
+			<view class="goods-right">
+				<view class="goods-name">{{goods.goods_name}}</view>
+				<view class="goods-info">
+					<view class="goods-price">￥{{goods.goods_price.toFixed(2)}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props: {
+			goods: {
+				type: Object,
+				default: {}
+			}
+		},
+		data() {
+			return {
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+	display: flex;
+	padding: 10px 5px;
+	border-bottom: 1px solid #efefef;
+	.goods-left {
+		padding-right: 5px;
+		image {
+			width: 100px;
+			height: 100px;
+		}
+	}
+	.goods-right {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods-name {
+			font-size: 13px;
+		}
+		.goods-price {
+			font-size: 16px;
+			color: #c00000;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		GoodsList
+		<view class="goods-list">
+			<view v-for="(item, index) in goodsList" :key="index" @click="gotoGoodsDetail(item)">
+				<my-goods :goods="item"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,9 +12,53 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cat_id: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cat_id = options.cid || ''
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(callback) {
+				this.isloading = true
+				const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				callback && callback()
+				this.isloading = false
+				console.log(res)
+				if(res.meta.status !== 200) return uni.$showMsg('请求有误')
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoGoodsDetail (item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('已经是最后一页')
+			if (this.isloading) return
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.goodsList = []
+			this.total = 0
+			this.isloading = false
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
+		
 	}
 </script>
 
